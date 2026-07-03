5898472f422dff33a66f4ae420f83801b1eb2000
rachhoya <virinarachhoya@gmail.com>
2026-07-03 18:23:04 +0100

    Update birthday_card.py

--- birthday_card.py
+++ birthday_card.py
@@ -10,6 +10,12 @@ pygame.display.set_caption("birthday card animation")
 image=pygame.image.load("balloon.jpg")
 image=pygame.transform.scale(image,(WIDTH,HEIGHT))
 
+image2=pygame.image.load("present.jpg")
+image2=pygame.transform.scale(image2,(WIDTH,HEIGHT))
+
+image3=pygame.image.load("cake.jpg")
+image3=pygame.transform.scale(image3,(WIDTH,HEIGHT))
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -19,6 +25,20 @@ while True:
         font=pygame.font.SysFont("Times New Roman",72)
         text=font.render("Happy",True,"black")
         text2=font.render("Birthday",True,"black")
-        screen.blit(text,(200,100))
-        screen.blit(text2,(200,200))
+        screen.blit(text,(207,125))
+        screen.blit(text2,(180,200))
         pygame.display.update()
+        time.sleep(8)
+
+        image2=pygame.image.load("present.jpg")
+        font1=pygame.font.SysFont("Times New Roman",38)
+        text3=font1.render("Wishing you a bright future ahead",True,"black")
+        screen.blit(image2,(0,0))
+        screen.blit(text3,(30,85))
+        pygame.display.update()
+        time.sleep(8)
+        
+        image3=pygame.image.load("cake.jpg")
+        screen.blit(image3,(0,0))
+        pygame.display.update()
+        time.sleep(8)
